f61730e4b7d87b31cdc5e9ccc2edde10806bee82
Liam Ramsey <liam0rams@gmail.com>
2023-04-29 20:45:05 -0500

    comments backend

---
+++ react/php/getComments.php
@@ -0,0 +1,55 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); 
+
+function getComments($input) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "kanm";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM comments WHERE show_id = $input";
+    $result = $conn->query($sql);
+    $rows = array();
+
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    echo json_encode($rows);
+
+    $conn->close();
+}
+
+// Check if the request method is GET
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Check if the function parameter is set and is a string
+    if (isset($_GET['function']) && is_string($_GET['function'])) {
+      // Retrieve the value of the function parameter
+      $function = filter_input(INPUT_GET, 'function', FILTER_SANITIZE_STRING);
+      
+      // Call the appropriate function based on the value of the function parameter
+      if ($function === 'getComments') {
+        // Call the getComments() function and return the result as a JSON object
+        if (isset($_GET['input'])) {
+          $input = filter_input(INPUT_GET, 'input', FILTER_SANITIZE_NUMBER_INT);
+          getComments($input);
+        } else {
+          echo json_encode(array('error' => 'Missing input parameter'));
+        }
+      } else {
+        echo json_encode(array('error' => 'Unknown function'));
+      }
+    } else {
+      echo json_encode(array('error' => 'Missing function parameter'));
+    }
+  }
+?>
--- react/src/pages/Shows.tsx
+++ react/src/pages/Shows.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Shows() {
   const { id } = useParams<{id: string}>();
+
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+      // Make a GET request to the PHP backend function
+      fetch('http://localhost/kanm-310/react/php/getComments.php?function=getComments&input=')
+      .then(response => response.json())
+      .then(data => setComments(data));
+  }, []);
+
+  type Comment = {
+      comment_id: number;
+      show_id: number;
+      time_stamp: string;
+      comment_text: string;
+    };      
+
+  console.log(comments);
+
   
   return (
     <div>
       <h1>Shows</h1>
       <p>ID: {id}</p>
+
+      {/* show stuff above this point, comments below */}
+
+
+
     </div>
   );
 }
