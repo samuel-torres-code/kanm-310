0e03c82734ac073cd593880567fd2c936d873e23
charrington24 <c.harrington.0301@gmail.com>
2023-05-02 14:50:58 -0500

    delete user and fixed icons

---
+++ react/php/deleteUser.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); 
+
+function deleteUser($user_id) {
+  
+    $servername = "localhost";
+    $uname = "root";
+    $pword = "";
+    $dbname = "kanm";
+    // Create connection
+    $conn = new mysqli($servername, $uname, $pword, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+  
+  //SQL query
+  $sql = "DELETE FROM users WHERE user_id = $user_id";
+  
+  // Execute the query
+  if (mysqli_query($conn, $sql)) {
+    echo "User deleted successfully";
+  } else {
+    echo "Error deleting user: " . mysqli_error($conn);
+  }
+  
+  // Close the connection
+  mysqli_close($conn);
+  
+}
+
+$data = json_decode(file_get_contents("php://input"));
+$user_id = $data->user_id;
+
+deleteUser($user_id);
+?>
